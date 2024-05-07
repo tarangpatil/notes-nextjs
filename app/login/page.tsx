6b5eedfb806a06ccdb91deabedb8e89e9f893aeb
Tarang Patil <72089368+tarangpatil@users.noreply.github.com>
2024-05-07 23:08:21 +0530

    Update page.tsx
    fix login redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 export default async function Page() {
   const session = await auth();
-  if (session) {
+  if (session && session.user) {
     redirect("/");
   }
   return (
